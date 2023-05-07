5b63101c3a74a351ac6557d3d546cce910779a23
Lepha <lepharamchiary@gmail.com>
2023-05-07 11:51:03 +0530

    .

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
-  const { data, loading } = useFetch("/hotels/countByCity?cities=berlin,madrid,london");
+  const { data, loading, error } = useFetch("/hotels/countByCity?cities=berlin,madrid,london");
     // console.log(data);
   return (
     <div className="featured">
